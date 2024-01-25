3538d528f15018408a8195c4adf77cee430ec9be
ForestAxe <droneuuuuu@gmail.com>
2024-01-25 23:29:57 +0300

    Implement Task-Submission React.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+        tasks: [],
+        firstValue: '',
+        secondValue: '',
+    };
+  }
+
+    handleInputChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8000/tasks/', {
+                "task": this.state.firstValue,
+                "title": this.state.secondValue,
+            });
+
+            console.log('Data: ', response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/tasks/')
+        .then(res =>{
+          this.setState({tasks: res.data})
+          console.log(res)
+        })
+        .catch(err =>{
+          console.log(err)
+        });
+  }
+
+  render() {
+    const tasks = this.state.tasks;
+    return(
+        <>
+            <div className="main-div">
+                <div className="app-container">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            type="text"
+                            name="firstValue"
+                            value={this.state.firstValue}
+                            onChange={this.handleInputChange}
+                            placeholder="Enter first value"
+                        />
+                        <input
+                            type="text"
+                            name="secondValue"
+                            value={this.state.secondValue}
+                            onChange={this.handleInputChange}
+                            placeholder="Enter second value"
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                    <div className="task-list">
+                        {RenderTasks(tasks)}
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+  }
+
+}
+
+function RenderTasks(tasks) {
+  return tasks.map((task, index) => ( // Iterate through tasks and display them vertically
+      <div key={index} className="task" > {/* Each task is given a border, padding, and margin */}
+        <div>ID: {index}</div>
+        <div>Title: {task.title}</div>
+        <div>Task: {task.task}</div>
+      </div>
+  ))
 }
 
 export default App;
--- schedulerApp/backend/urls.py
+++ schedulerApp/backend/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TasksApiView
 
 urlpatterns = [
-    path('', TasksApiView.as_view(), name="anything")
+    path('tasks/', TasksApiView.as_view(), name="anything")
 ]
